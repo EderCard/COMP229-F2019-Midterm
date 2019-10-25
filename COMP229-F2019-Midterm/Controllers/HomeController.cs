5dc5455954ab918ce2a0130b85c32f0f9b579581
EderCard <edersoncard@gmail.com>
2019-10-24 22:43:59 -0400

    Initial setup

---
+++ COMP229-F2019-Midterm/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using COMP229_F2019_Midterm.Models;
+
+namespace COMP229_F2019_Midterm.Controllers
+{
+    public class HomeController : Controller
+    {
+        // default action method
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ COMP229-F2019-Midterm/Models/Repository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COMP229_F2019_Midterm.Models
+{
+    public class Repository
+    {
+
+    }
+}
